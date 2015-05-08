f9a14ee44b993ad14a725419c6aa6fde635204f6
Raluca06 <ralu_cutza106721@yahoo.com>
2015-05-08 17:25:11 +0300

    [~] added code for creating the User table and the database

---
+++ app/src/main/java/com/android/nutritious/nutritious/UserContract.java
@@ -0,0 +1,17 @@
+package com.android.nutritious.nutritious;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Ralu on 5/8/2015.
+ */
+public class UserContract {
+
+    public static final class UserEntry implements BaseColumns{
+        public static final String TABLE_NAME = "user";
+    }
+
+    public static final class FoodEntry implements BaseColumns{
+        public static final String TABLE_NAME = "food";
+    }
+}
