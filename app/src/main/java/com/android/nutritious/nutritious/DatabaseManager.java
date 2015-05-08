00768a4658067667e23cc2443cee190622e45561
Raluca06 <ralu_cutza106721@yahoo.com>
2015-05-08 17:02:41 +0300

    [~] added code for creating the User table and the database

--- app/src/main/java/com/android/nutritious/nutritious/DatabaseManager.java
+++ app/src/main/java/com/android/nutritious/nutritious/DatabaseManager.java
@@ -59,4 +59,5 @@ public class DatabaseManager extends SQLiteOpenHelper {
             onCreate(db);
         }
     }
+
 }
--- app/src/main/java/com/android/nutritious/nutritious/OneTimeStarterActivity.java
+++ app/src/main/java/com/android/nutritious/nutritious/OneTimeStarterActivity.java
@@ -8,10 +8,13 @@ import android.view.MenuItem;
 
 public class OneTimeStarterActivity extends ActionBarActivity {
 
+    private DatabaseManager dbManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_time_starter);
+       // dbManager = new DatabaseManager(mContext);
     }
 
 
