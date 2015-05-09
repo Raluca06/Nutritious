0b98b8b3d7ca1cf6123026ea5480f06a388e0599
Raluca06 <ralu_cutza106721@yahoo.com>
2015-05-09 03:20:18 +0300

    Adding the toggle for launcher activities.

---
+++ app/src/main/java/com/android/nutritious/nutritious/ToggleActivity.java
@@ -0,0 +1,53 @@
+package com.android.nutritious.nutritious;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class ToggleActivity extends Activity {
+
+    private int toggle = 2;
+    Context context = this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.toggle_activity);
+        Intent newActivity;
+        if(toggle == 1) {
+            newActivity = new Intent(context, OneTimeStarterActivity.class);
+        } else {
+            newActivity = new Intent(context, MainActivity.class);
+        }
+        context.startActivity(newActivity);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toggle_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
