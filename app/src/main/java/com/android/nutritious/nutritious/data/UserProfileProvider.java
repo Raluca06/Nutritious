f2482480bff8960e02318636e92425f419e871be
Raluca06 <ralu_cutza106721@yahoo.com>
2015-05-10 02:38:22 +0300

    Extending the ContentProvider and implementing the according methods.

--- app/src/main/java/com/android/nutritious/nutritious/data/UserProfileProvider.java
+++ app/src/main/java/com/android/nutritious/nutritious/data/UserProfileProvider.java
@@ -1,7 +1,41 @@
 package com.android.nutritious.nutritious.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
 /**
  * Created by Ralu on 10.05.2015.
  */
-public class UserProfileProvider {
+public class UserProfileProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
