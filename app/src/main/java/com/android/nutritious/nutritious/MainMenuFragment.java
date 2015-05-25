c9264838025b5db5016f18109535c79806386b05
Raluca06 <ralu_cutza106721@yahoo.com>
2015-05-25 21:29:27 +0300

    Added all the other activities that emerge from the main menu of the app.

--- app/src/main/java/com/android/nutritious/nutritious/MainMenuFragment.java
+++ app/src/main/java/com/android/nutritious/nutritious/MainMenuFragment.java
@@ -1,13 +1,16 @@
 package com.android.nutritious.nutritious;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,6 +35,37 @@ public class MainMenuFragment extends Fragment {
                 getActivity(), R.layout.fragment_list_item_mainmenu, R.id.list_item_mainMenu, menuOptions);
         ListView listView = (ListView) rootView.findViewById(R.id.list_mainMenu);
         listView.setAdapter(mMainMenuAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String menuOption = mMainMenuAdapter.getItem(position);
+                switch (position){
+                    case 0:{
+                        Intent intent = new Intent(getActivity(), FoodFinderActivity.class).putExtra(Intent.EXTRA_TEXT, menuOption);
+                        startActivity(intent);
+                    }
+                        break;
+                    case 1:{
+                        Intent intent = new Intent(getActivity(), PersonalProfileActivity.class).putExtra(Intent.EXTRA_TEXT, menuOption);
+                        startActivity(intent);
+                    }
+                        break;
+                    case 2:{
+                        Intent intent = new Intent(getActivity(), DrinksCounterActivity.class).putExtra(Intent.EXTRA_TEXT, menuOption);
+                        startActivity(intent);
+                    }
+                        break;
+                    default:{
+                        Intent intent = new Intent(getActivity(), StatisticsActivity.class).putExtra(Intent.EXTRA_TEXT, menuOption);
+                        startActivity(intent);
+                    }
+
+                        break;
+                }
+            }
+        });
         return rootView;
     }
 
